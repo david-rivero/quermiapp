ab4083211bfa25b0efa29a39b60098311e8fe81d
David Rivero <david.davidsociety.rivero@gmail.com>
2020-10-02 21:15:59 -0300

    Change android conf

--- android/app/src/main/java/com/quermiapp/MainApplication.java
+++ android/app/src/main/java/com/quermiapp/MainApplication.java
@@ -13,7 +13,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-//import com.filepicker.FilePickerPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -30,8 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new FilePickerPackage());
           return packages;
         }
 
